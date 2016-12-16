9ffde85d39db69ebb720dbdb09155bcc9f446df1
argyn <rauan.argyn@gmail.com>
2016-12-16 11:37:51 +0100

    Loggin disabled for submission

--- src/Logger.java
+++ src/Logger.java
@@ -11,25 +11,25 @@ public enum Logger {
   private java.util.logging.Logger logger;
 
   private Logger() {
-    logger = java.util.logging.Logger.getLogger("MKAgent");
-    try {
-      logger.setUseParentHandlers(false);
-      String currentPath = Paths.get(".").toAbsolutePath().normalize().toString();
-      String path = String.format("%s/%s", currentPath, "MKAgent.log");
-      FileHandler fh = new FileHandler(path);
-      logger.addHandler(fh);
-      SimpleFormatter formatter = new SimpleFormatter();
-      fh.setFormatter(formatter);
-    } catch(Exception e) {
-      e.printStackTrace();
-    }
+    // logger = java.util.logging.Logger.getLogger("MKAgent");
+    // try {
+    //   logger.setUseParentHandlers(false);
+    //   String currentPath = Paths.get(".").toAbsolutePath().normalize().toString();
+    //   String path = String.format("%s/%s", currentPath, "MKAgent.log");
+    //   FileHandler fh = new FileHandler(path);
+    //   logger.addHandler(fh);
+    //   SimpleFormatter formatter = new SimpleFormatter();
+    //   fh.setFormatter(formatter);
+    // } catch(Exception e) {
+    //   e.printStackTrace();
+    // }
   }
 
   public void info(String message) {
-    logger.info(message);
+    //logger.info(message);
   }
 
   public void severe(String message) {
-    logger.severe(message);
+    //logger.severe(message);
   }
 }
